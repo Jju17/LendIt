3e4fb93655061b45a141bc56860fcfc828ccc98c
julien.rahier <julien.rahier@persgroep.net>
2025-03-20 14:34:43 +0100

    Initial Commit

---
+++ LendIt/LendItApp.swift
@@ -0,0 +1,17 @@
+//
+//  LendItApp.swift
+//  LendIt
+//
+//  Created by Julien Rahier on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct LendItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
