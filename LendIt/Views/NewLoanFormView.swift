72030683093450f75c452927e7f97eb71c1e2f14
julien.rahier <julien.rahier@gmail.com>
2025-04-20 17:23:16 +0200

    Add new fields to "new lend" form

--- LendIt/Views/NewLoanFormView.swift
+++ LendIt/Views/NewLoanFormView.swift
@@ -39,6 +39,24 @@ struct NewLoanFormView: View {
         Form {
             Section {
                 TextField("Loan name", text: $store.wipLoan.name)
+                TextField("Borrower name", text: $store.wipLoan.borrowerName)
+                GeometryReader { geometry in
+                    HStack {
+                        Text("Start date")
+                        Spacer()
+                        HStack(spacing: 0) {
+                            Button("Today") { store.wipLoan.startDate = Date() }
+                                .buttonStyle(.bordered)
+                                .padding(.horizontal, 0)
+                            DatePicker(selection: $store.wipLoan.startDate, displayedComponents: .date) {
+                                EmptyView()
+                            }
+                            .padding(.horizontal, 0)
+                        }
+                        .frame(width: geometry.size.width * 0.6, alignment: .trailing)
+                    }
+                }
+                DatePicker("End date", selection: $store.wipLoan.endDate, in: store.wipLoan.startDate..., displayedComponents: .date)
             }
         }
     }
